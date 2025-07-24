b040535d1b9a1da13ee69c02df7ea427ec054527
Stephen-Beck <stephenbeck89@outlook.com>
2025-07-24 19:15:26 -0400

    Add project files.

---
+++ Form1.Designer.cs
@@ -0,0 +1,121 @@
+﻿namespace Assignment_3._3
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            components = new System.ComponentModel.Container();
+            dataGridView1 = new DataGridView();
+            studentIDDataGridViewTextBoxColumn = new DataGridViewTextBoxColumn();
+            firstNameDataGridViewTextBoxColumn = new DataGridViewTextBoxColumn();
+            lastNameDataGridViewTextBoxColumn = new DataGridViewTextBoxColumn();
+            addressDataGridViewTextBoxColumn = new DataGridViewTextBoxColumn();
+            monthOfAdmissionDataGridViewTextBoxColumn = new DataGridViewTextBoxColumn();
+            gradeDataGridViewTextBoxColumn = new DataGridViewTextBoxColumn();
+            studentBindingSource = new BindingSource(components);
+            ((System.ComponentModel.ISupportInitialize)dataGridView1).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)studentBindingSource).BeginInit();
+            SuspendLayout();
+            // 
+            // dataGridView1
+            // 
+            dataGridView1.AutoGenerateColumns = false;
+            dataGridView1.AutoSizeColumnsMode = DataGridViewAutoSizeColumnsMode.Fill;
+            dataGridView1.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            dataGridView1.Columns.AddRange(new DataGridViewColumn[] { studentIDDataGridViewTextBoxColumn, firstNameDataGridViewTextBoxColumn, lastNameDataGridViewTextBoxColumn, addressDataGridViewTextBoxColumn, monthOfAdmissionDataGridViewTextBoxColumn, gradeDataGridViewTextBoxColumn });
+            dataGridView1.DataSource = studentBindingSource;
+            dataGridView1.Dock = DockStyle.Fill;
+            dataGridView1.Location = new Point(0, 0);
+            dataGridView1.Name = "dataGridView1";
+            dataGridView1.Size = new Size(800, 450);
+            dataGridView1.TabIndex = 0;
+            // 
+            // studentIDDataGridViewTextBoxColumn
+            // 
+            studentIDDataGridViewTextBoxColumn.DataPropertyName = "StudentID";
+            studentIDDataGridViewTextBoxColumn.HeaderText = "StudentID";
+            studentIDDataGridViewTextBoxColumn.Name = "studentIDDataGridViewTextBoxColumn";
+            // 
+            // firstNameDataGridViewTextBoxColumn
+            // 
+            firstNameDataGridViewTextBoxColumn.DataPropertyName = "FirstName";
+            firstNameDataGridViewTextBoxColumn.HeaderText = "FirstName";
+            firstNameDataGridViewTextBoxColumn.Name = "firstNameDataGridViewTextBoxColumn";
+            // 
+            // lastNameDataGridViewTextBoxColumn
+            // 
+            lastNameDataGridViewTextBoxColumn.DataPropertyName = "LastName";
+            lastNameDataGridViewTextBoxColumn.HeaderText = "LastName";
+            lastNameDataGridViewTextBoxColumn.Name = "lastNameDataGridViewTextBoxColumn";
+            // 
+            // addressDataGridViewTextBoxColumn
+            // 
+            addressDataGridViewTextBoxColumn.DataPropertyName = "Address";
+            addressDataGridViewTextBoxColumn.HeaderText = "Address";
+            addressDataGridViewTextBoxColumn.Name = "addressDataGridViewTextBoxColumn";
+            // 
+            // monthOfAdmissionDataGridViewTextBoxColumn
+            // 
+            monthOfAdmissionDataGridViewTextBoxColumn.DataPropertyName = "MonthOfAdmission";
+            monthOfAdmissionDataGridViewTextBoxColumn.HeaderText = "MonthOfAdmission";
+            monthOfAdmissionDataGridViewTextBoxColumn.Name = "monthOfAdmissionDataGridViewTextBoxColumn";
+            // 
+            // gradeDataGridViewTextBoxColumn
+            // 
+            gradeDataGridViewTextBoxColumn.DataPropertyName = "Grade";
+            gradeDataGridViewTextBoxColumn.HeaderText = "Grade";
+            gradeDataGridViewTextBoxColumn.Name = "gradeDataGridViewTextBoxColumn";
+            // 
+            // studentBindingSource
+            // 
+            studentBindingSource.DataSource = typeof(Student);
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 450);
+            Controls.Add(dataGridView1);
+            Name = "Form1";
+            Text = "Student Information";
+            ((System.ComponentModel.ISupportInitialize)dataGridView1).EndInit();
+            ((System.ComponentModel.ISupportInitialize)studentBindingSource).EndInit();
+            ResumeLayout(false);
+        }
+
+        #endregion
+
+        private DataGridView dataGridView1;
+        private BindingSource studentBindingSource;
+        private DataGridViewTextBoxColumn studentIDDataGridViewTextBoxColumn;
+        private DataGridViewTextBoxColumn firstNameDataGridViewTextBoxColumn;
+        private DataGridViewTextBoxColumn lastNameDataGridViewTextBoxColumn;
+        private DataGridViewTextBoxColumn addressDataGridViewTextBoxColumn;
+        private DataGridViewTextBoxColumn monthOfAdmissionDataGridViewTextBoxColumn;
+        private DataGridViewTextBoxColumn gradeDataGridViewTextBoxColumn;
+    }
+}
---
+++ Student.cs
@@ -0,0 +1,36 @@
+﻿namespace Assignment_3._3
+{
+    public class Student
+    {
+        public enum Month { Unknown = 0, Jan = 1, Feb, Mar, Apr, May, Jun, Jul, Aug, Sep, Oct, Nov, Dec }
+        private static int studentID =1;
+        public int StudentID { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Address { get; set; }
+        public Month MonthOfAdmission { get; set; }
+        public char Grade { get; set; }
+
+        public Student()
+        {
+            StudentID = studentID++;
+        }
+
+        public static List<Student> PopulateStudents()
+        {
+            return new List<Student>()
+            {
+                new Student() { FirstName = "Mark",  LastName = "Meta",     Address = "Brooklyn, NY", MonthOfAdmission = Month.Aug, Grade = 'F'},
+                new Student() { FirstName = "Linus", LastName = "Linux",    Address = "Brooklyn, NY", MonthOfAdmission = Month.Jun, Grade = 'A'},
+                new Student() { FirstName = "Val",   LastName = "Gaben",    Address = "Brooklyn, NY", MonthOfAdmission = Month.Jul, Grade = 'A'},
+                new Student() { FirstName = "Billy", LastName = "Windows",  Address = "Brooklyn, NY", MonthOfAdmission = Month.Aug, Grade = 'B'},
+                new Student() { FirstName = "Tim",   LastName = "Apple",    Address = "Brooklyn, NY", MonthOfAdmission = Month.Aug, Grade = 'C'},
+                new Student() { FirstName = "Jack",  LastName = "Twitter",  Address = "Brooklyn, NY", MonthOfAdmission = Month.Sep, Grade = 'C'},
+                new Student() { FirstName = "Sammy", LastName = "Crypto",   Address = "Brooklyn, NY", MonthOfAdmission = Month.Aug, Grade = 'F'},
+                new Student() { FirstName = "Larry", LastName = "O'Racle",  Address = "Brooklyn, NY", MonthOfAdmission = Month.Jul, Grade = 'A'},
+                new Student() { FirstName = "Bill",  LastName = "Snow",     Address = "Brooklyn, NY", MonthOfAdmission = Month.Jul, Grade = 'D'},
+                new Student() { FirstName = "Paul",  LastName = "Anteer",   Address = "Brooklyn, NY", MonthOfAdmission = Month.Aug, Grade = 'B'}
+            };
+        }
+    }
+}
